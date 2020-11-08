607e83d4d9d1d93de9544173efeecb6bfeec89ce
haruhiko28 <not.hisano.soccer@gmail.com>
2020-11-08 16:08:14 +0900

    hoge

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -275,9 +275,163 @@ fn main() {
     // write!(&mut w, "{}", "ABC");
     // writeln!(&mut w, " is 123");
     // dbg!(w);
+
+    // panic!("it will panic");
+
+    // let v = vec![1,2,3];
+
+    println!("defined in file: {}",file!());
+    println!("defined on line: {}",line!());
+    println!("is test: {}",cfg!(unix));
+    println!("CARGO_HOME: {}", env!("CARGO_HOME"));
+
+    // アサーション用のマクロ
+    // assert!(true);
+    // assert_eq!(1,1);
+    // assert_ne!(1,0);
+    // debug_assert!(false);
+    // debug_assert_eq!(1,1);
+    // debug_assert_ne!(1,0);
+
+    // 実装補助用のマクロ
+    // let mut p = HappyPerson{
+    //     name: "Takashi".to_string(),
+    //     state: Emotion::Happy,
+    // };
+
+    // println!("{}", p.get_happy());
+
+    //トレイトの導出
+    println!("{:?}", A(0)==A(1));
+
+    println!("{:?}", B(1.0) > B(0.0));
+
+    let c0 = C;
+    let _c1 = c0;
+    let _c2 = c0;
+
+    let d0 = D;
+    let _d1 = d0.clone();
+
+    println!("{:?}",E);
+
+    let _f = F::default();
+    
+    // traitとdyn
+    let dove = Dove {};
+    dove.tweet();
+    dove.tweet_twice();
+    dove.shout();
+
+    let duck = Duck {};
+
+    let bird_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dove),Box::new(duck)];
+    for bird in bird_vec{
+        bird.tweet();
+    }
+
+    // ジェネリクス
+    let t1 = make_tuple(1, 2);
+    let t2 = make_tuple("Hello", "World");
+    let t3 = make_tuple(vec![1,2,3], vec![4,5]);
+    let t4 = make_tuple(3, "years old");
+    println!("{:?}", t1);
+    println!("{:?}", t2);
+    println!("{:?}", t3);
+    println!("{:?}", t4);
 }
 // ======================================================================================================================================================
+// ジェネリクス
+fn make_tuple<T,S>(t: T, s: S) -> (T,S){
+    (t,s)
+}
 
+// traitとdyn
+trait Tweet {
+    fn tweet(&self);
+
+    fn tweet_twice(&self){
+        self.tweet();
+        self.tweet();
+    }
+
+    fn shout(&self){
+        println!("Uoooooohhhh!!");
+    }
+}
+
+struct Dove;
+struct Duck;
+
+
+impl Tweet for Dove {
+    fn tweet(&self){
+        println!("Coo!");
+    }
+}
+
+impl Tweet for Duck{
+    fn tweet(&self){
+        println!("Quack");
+    }
+}
+// トレイトの導出
+#[derive(Eq, PartialEq)]
+struct A(i32);
+
+#[derive(PartialEq, PartialOrd)]
+struct B(f32);
+
+#[derive(Copy, Clone)]
+struct C;
+
+#[derive(Clone)]
+struct D;
+
+#[derive(Debug)]
+struct E;
+
+#[derive(Default)]
+struct F;
+
+
+// unreachableの関数の書き方
+// fn f(x: usize) -> &'static str{
+//     match x {
+//         n if n * n % 3 == 0 => "3n",
+//         n if n * n % 3 == 1 => "3n+1 or 3n+2",
+//         _ => unreachable!(),
+//     }
+// }
+
+// 実装補助用のマクロ
+// enum Emotion{
+//     // Anger,
+//     Happy,
+// }
+
+// trait Emotional{
+//     fn get_happy(&mut self) -> String;
+//     fn get_anger(&mut self) -> String;
+//     fn tell_state(&self) -> String;
+// }
+
+// struct HappyPerson{
+//     name: String,
+//     state: Emotion,
+// }
+
+// impl Emotional for HappyPerson{
+//     fn get_anger(&mut self) -> String {
+//         unimplemented!()
+//     }
+//     fn get_happy(&mut self) -> String {
+//         format!("{} is always happy.",self.name)
+//     }
+//     fn tell_state(&self) -> String {
+//         todo!()
+//     }
+// }
 //impl
 struct Person3{
     name: String,
