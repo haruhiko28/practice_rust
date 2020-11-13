f0d5bce821241bf998ba085922348a1c99bfc919
haruhiko28 <not.hisano.soccer@gmail.com>
2020-11-13 20:20:38 +0900

    昨日までの分

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,5 +1,8 @@
 // use std::io::Write; // write, writelnマクロを使うため
 use std::thread;
+// use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 
 fn main() {
     println!("Hello, world!");
@@ -397,11 +400,13 @@ fn main() {
 
     //スレッドを作る 不確定性がある
     let mut handles = Vec::new();
-    let mut data = vec![1;10];
+    let data = Arc::new(Mutex::new(vec![1;10]));
 
     for x in 0..10{
-        handles.push(thread::spawn(move ||{
-            println!("Hello, world! {}",x);
+        let data_ref = data.clone();
+        handles.push(thread::spawn(move || {
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
         }))
     }
 
@@ -410,6 +415,52 @@ fn main() {
     }
 
     dbg!(data);
+
+    let (tx, rx) = mpsc::channel();
+    let handle = thread::spawn(move ||{
+        let data = rx.recv().unwrap();
+        println!("{}",data);
+    });
+
+    let _ = tx.send("Hello, world!");
+
+    let _ = handle.join();
+
+    let mut handles = Vec::new();
+    let mut data = vec![1; 10];
+    let mut snd_channels = Vec::new();
+    let mut rcv_channels = Vec::new();
+
+    for _ in 0..10{
+        // mainから各スレッドへのチャンネル
+        let (snd_tx, snd_rx) = mpsc::channel();
+        // 各スレッドからmainへのチャンネル
+        let (rcv_tx, rcv_rx) = mpsc::channel();
+
+        snd_channels.push(snd_tx);
+        rcv_channels.push(rcv_rx);
+
+        handles.push(thread::spawn(move ||{
+            let mut data = snd_rx.recv().unwrap();
+            data += 1;
+            let _ = rcv_tx.send(data);
+        }));
+    }
+        //各スレッドにdataの値を送信
+        for x in 0..10{
+            let _ = snd_channels[x].send(data[x]);
+        }
+
+        for x in 0..10{
+            data[x] = rcv_channels[x].recv().unwrap();
+        }
+
+        for handle in handles{
+            let _ = handle.join();
+        }
+
+        dbg!(data);
+
 
 }
 // ======================================================================================================================================================
