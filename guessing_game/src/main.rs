68742c392d2e28a216566ce6e7a8046f3ee513c9
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-11 22:57:02 +0900

    add ordering

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,31 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("Please input your guess.");
-    
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Fail to read line!");
-    
-    println!("You guessed : {}", guess);
+    loop {
+        println!("Please input your guess.");    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Fail to read line!");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("small"),
+            Ordering::Greater => println!("large"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        println!("You guessed : {}", guess);
+    }
 }
