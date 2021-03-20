98af3e713eada207289c6d490f1d89a453b2c361
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-20 17:37:25 +0900

    四則演算

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    let spaces = "  ";
-    let spaces = spaces.len();
+    let sum = 5 + 10;
 
-    println!("The value of spaces is {}", spaces); // x = 12
+    let diff = 5 - 10;
 
-    // 手数はconstと全て大文字でアンダースコアで単語区切り
-    const MAX_POINTS: u32 = 100_000;
-    println!("The value of MAX_POINT is {}", MAX_POINTS);
+    let prod = 5 * 10;
+
+    let quot = 5 % 10;
+
+    let remai = 5 % 10;
 }
