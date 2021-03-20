82e8b2d3414a5f1141082eddf75ce6433042747d
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-20 18:23:06 +0900

    関数周り

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let x = 5;
+    let x = five();
+    println!("The value of x is: {}", x);
+}
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
-    println!("The value of y is: {}", y);
+fn five() -> i32 {
+    5
 }
