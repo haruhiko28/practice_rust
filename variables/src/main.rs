d9cfa6209fc421651c259a19a8dded873daec5eb
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-20 17:31:21 +0900

    letで再定義

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {}", x);
-    x = 6;
-    println!("The value of x is {}", x);
+    let x = 5;
+    
+    let x = x + 1;
+    
+    let x = x*2;
+
+    println!("The value of x is {}", x); // x = 12
 
     // 手数はconstと全て大文字でアンダースコアで単語区切り
     const MAX_POINTS: u32 = 100_000;
