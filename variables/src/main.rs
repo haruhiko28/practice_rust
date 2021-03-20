eae9f35ba1ff69aef95f1d02e7de8dc12c7b2386
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-20 17:26:47 +0900

    定数の書き方

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+
+    // 手数はconstと全て大文字でアンダースコアで単語区切り
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINT is {}", MAX_POINTS);
 }
