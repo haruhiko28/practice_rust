831e5a3555c0744983a87dfeab79866b36fa2429
haruhiko28 <not.hisano.soccer@gmail.com>
2021-03-20 18:04:57 +0900

    関数と文の話

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let sum = 5 + 10;
+    let x = 5;
 
-    let diff = 5 - 10;
-
-    let prod = 5 * 10;
-
-    let quot = 5 % 10;
-
-    let remai = 5 % 10;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
 }
